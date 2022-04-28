de276b8fd60ca55b51a4de1ac60d9ae28c035cc4
mengzihan <u7354208@anu.edu.au>
2022-04-29 01:00:56 +0800

    Establish a basic login page structure

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testButton = (Button) findViewById(R.id.testButton);
+
+        //Set a test button to login page
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginActivity= new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(loginActivity);
+            }
+        });
+
+
     }
 }

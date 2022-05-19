1cff2772779c425c055b5c13397563ad4d53b0e7
mengzihan <u7354208@anu.edu.au>
2022-05-19 18:51:21 +0800

    add layout to ManageAccountActivity

--- app/src/main/java/com/example/myfirstapp/ManageAccountActivity.java
+++ app/src/main/java/com/example/myfirstapp/ManageAccountActivity.java
@@ -3,12 +3,47 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+/**
+ * @author Zihan Meng
+ * @author Enze Peng
+ */
 public class ManageAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_account);
+
+        //set title to "MyPost"
+        getSupportActionBar().setTitle("Manage Account");
+
+        //Create an ArrayList to store URLs
+        ArrayList<String> accounts = new ArrayList<>();
+        accounts.add("Simon");
+        accounts.add("EricCarmelo");
+        accounts.add("Carmelo");
+        accounts.add("Arthur");
+
+        //Create Adapter for listView
+        ListView myAccountsList = (ListView) findViewById(R.id.accounts_list);
+        ArrayAdapter myAccountssAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,accounts);
+        myAccountsList.setAdapter(myAccountssAdapter);
+
+        //when long click on items of list
+        myAccountsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String currentClicked = accounts.get(i);
+                Toast.makeText(ManageAccountActivity.this,"You long clicked "+currentClicked,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
